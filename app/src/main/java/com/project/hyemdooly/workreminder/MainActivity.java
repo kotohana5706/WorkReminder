be2c508694636bdac9fbd176d2c98ae634dd3b07
hyemdooly <hyemdooly@gmail.com>
2016-09-15 00:13:12 +0900

    recyclerView commit

--- app/src/main/java/com/project/hyemdooly/workreminder/MainActivity.java
+++ app/src/main/java/com/project/hyemdooly/workreminder/MainActivity.java
@@ -2,28 +2,51 @@ package com.project.hyemdooly.workreminder;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private String[] textData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mRecyclerView = (RecyclerView) findViewById(R.id.main_recycler_view);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.AddWorkfab);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.add_work_fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Toast.makeText(getApplicationContext(),"Add your Work!", Toast.LENGTH_SHORT);
+                // getApplicationContext : 현재 실행되고 있는 앱의 실행 환경
+                // 다이얼로그
             }
         });
+
+        // 하나의 리스트/카드 사이즈를 일정하게하겠습니다 - true
+        mRecyclerView.setHasFixedSize(true);
+
+
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        mAdapter = new RecyclerAdapter(textData);
+        mRecyclerView.setAdapter(mAdapter);
+
+
     }
 
+
+
 }
+
