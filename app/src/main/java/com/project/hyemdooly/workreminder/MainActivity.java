a44bf5baf2f87484f87bc97d25d32af8cbcb8fe0
hyemdooly <hyemdooly@gmail.com>
2016-09-19 00:47:28 +0900

    Commit!!
    캘린더 날짜 선택 후 Date칸에 날짜가 뜨도록 수정

--- app/src/main/java/com/project/hyemdooly/workreminder/MainActivity.java
+++ app/src/main/java/com/project/hyemdooly/workreminder/MainActivity.java
@@ -19,6 +19,7 @@ public class MainActivity extends AppCompatActivity implements CalendarDatePicke
 
     public EditText dialogDateTime;
     private static final String FRAG_TAG_DATE_PICKER = "Select Date";
+    private View dialogView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,37 +35,34 @@ public class MainActivity extends AppCompatActivity implements CalendarDatePicke
                 Toast.makeText(getApplicationContext(),"Add your Work!", Toast.LENGTH_SHORT);
                 // getApplicationContext : 현재 실행되고 있는 앱의 실행 환경
                 LayoutInflater inflater = getLayoutInflater();
-                final View dialogView = inflater.inflate(R.layout.add_work_dialog, null);
+                dialogView = inflater.inflate(R.layout.add_work_dialog, null);
                 // null 자리는 ViewGroup root
                 AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this); // Activity가 Context를 상속받음
                 builder.setView(dialogView).show();
+                dialogDateTime = (EditText) dialogView.findViewById(R.id.dialog_date_time);
+
+                ////여기서부터 문제생김
+
+                dialogDateTime.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        CalendarDatePickerDialogFragment cdp = new CalendarDatePickerDialogFragment()
+                                .setOnDateSetListener(MainActivity.this)
+                                .setFirstDayOfWeek(Calendar.SUNDAY);
+                        cdp.show(getSupportFragmentManager(), FRAG_TAG_DATE_PICKER);
+
+                    }
+                });
             }
         });
-
-        EditText dialogDateTime = (EditText) findViewById(R.id.dialog_date_time);
-        ////여기서부터 문제생김
-        
-        dialogDateTime.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                CalendarDatePickerDialogFragment cdp = new CalendarDatePickerDialogFragment()
-                        .setOnDateSetListener(MainActivity.this)
-                        .setFirstDayOfWeek(Calendar.SUNDAY);
-                cdp.show(getSupportFragmentManager(), FRAG_TAG_DATE_PICKER);
-            }
-        });
-
-
 
     }
+
 
     @Override
     public void onDateSet(CalendarDatePickerDialogFragment dialog, int year, int monthOfYear, int dayOfMonth){
-        dialogDateTime.setText(getString(R.string.calendar_date_picker_result_values, year, monthOfYear, dayOfMonth));
+        dialogDateTime.setText(dialog.getResources().getString(R.string.calendar_date_picker_result_values, year, monthOfYear, dayOfMonth));
     }
-
-
-
 
 
 
