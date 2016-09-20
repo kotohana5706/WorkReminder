b2a6c745be2143c1809ce472e98b9f83d2506104
hyemdooly <hyemdooly@gmail.com>
2016-09-20 21:38:34 +0900

    I'm...So...Serious.....

---
+++ app/src/main/java/com/project/hyemdooly/workreminder/DialogAddWork.java
@@ -0,0 +1,96 @@
+package com.project.hyemdooly.workreminder;
+
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.codetroopers.betterpickers.calendardatepicker.CalendarDatePickerDialogFragment;
+import com.codetroopers.betterpickers.radialtimepicker.RadialTimePickerDialogFragment;
+
+import java.util.Calendar;
+
+public class DialogAddWork extends AppCompatActivity implements CalendarDatePickerDialogFragment.OnDateSetListener, RadialTimePickerDialogFragment.OnTimeSetListener {
+
+    private EditText dialogDate;
+    private EditText dialogTime;
+    private Button buttonOk;
+    private Button buttonCancel;
+    private static final String FRAG_TAG_DATE_PICKER = "Select Date";
+    private static final String FRAG_TAG_TIME_PICKER = "Select Time";
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        dialogDate = (EditText) findViewById(R.id.dialog_time);
+        dialogTime = (EditText) findViewById(R.id.dialog_date);
+        buttonOk = (Button) findViewById(R.id.dialog_ok_button);
+        buttonCancel = (Button) findViewById(R.id.dialog_cancel_button);
+    }
+
+    public void showDialog(View v){
+
+        final AlertDialog addWorkDialog = new AlertDialog.Builder(this).create();
+        addWorkDialog.setView(v);
+        addWorkDialog.show();
+
+        dialogDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // See Calendar
+                showDatePicker(DialogAddWork.this);
+            }
+        });
+
+        dialogTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // See Clock
+                showTimePicker(DialogAddWork.this);
+            }
+        });
+
+        buttonOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addWorkDialog.dismiss();
+            }
+        });
+
+        buttonCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addWorkDialog.dismiss();
+            }
+        });
+
+
+    }
+
+    public void showDatePicker(CalendarDatePickerDialogFragment.OnDateSetListener onDateSetListener){
+        CalendarDatePickerDialogFragment cdp = new CalendarDatePickerDialogFragment()
+                .setOnDateSetListener(onDateSetListener)
+                .setFirstDayOfWeek(Calendar.SUNDAY);
+        cdp.show(getSupportFragmentManager(), FRAG_TAG_DATE_PICKER);
+    }
+
+    public void showTimePicker(RadialTimePickerDialogFragment.OnTimeSetListener onTimeSetListener){
+        RadialTimePickerDialogFragment rtpd = new RadialTimePickerDialogFragment()
+                .setOnTimeSetListener(onTimeSetListener)
+                .setForced12hFormat();
+        rtpd.show(getSupportFragmentManager(), FRAG_TAG_TIME_PICKER);
+    }
+
+    @Override
+    public void onDateSet(CalendarDatePickerDialogFragment dialog, int year, int monthOfYear, int dayOfMonth) {
+        dialogDate.setText(dialog.getResources().getString(R.string.calendar_date_picker_result_values, year, monthOfYear, dayOfMonth));
+    }
+
+    @Override
+    public void onTimeSet(RadialTimePickerDialogFragment dialog, int hourOfDay, int minute) {
+        dialogTime.setText(dialog.getResources().getString(R.string.calendar_time_picker_result_values, hourOfDay, minute));
+    }
+}
--- app/src/main/java/com/project/hyemdooly/workreminder/MainActivity.java
+++ app/src/main/java/com/project/hyemdooly/workreminder/MainActivity.java
@@ -2,26 +2,13 @@ package com.project.hyemdooly.workreminder;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.Toast;
 
-import com.codetroopers.betterpickers.calendardatepicker.CalendarDatePickerDialogFragment;
-import com.codetroopers.betterpickers.radialtimepicker.RadialTimePickerDialogFragment;
-
-import java.util.Calendar;
-
-public class MainActivity extends AppCompatActivity implements CalendarDatePickerDialogFragment.OnDateSetListener, RadialTimePickerDialogFragment.OnTimeSetListener {
-
-
-    public EditText dialogDate;
-    private EditText dialogTime;
-    private static final String FRAG_TAG_DATE_PICKER = "Select Date";
-    private static final String FRAG_TAG_TIME_PICKER = "Select Time";
+public class MainActivity extends AppCompatActivity {
     private View dialogView;
 
     @Override
@@ -35,37 +22,17 @@ public class MainActivity extends AppCompatActivity implements CalendarDatePicke
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(getApplicationContext(),"Add your Work!", Toast.LENGTH_SHORT);
+                Toast.makeText(getApplicationContext(), "Add your Work!", Toast.LENGTH_SHORT);
                 // getApplicationContext : 현재 실행되고 있는 앱의 실행 환경
+
                 LayoutInflater inflater = getLayoutInflater();
                 dialogView = inflater.inflate(R.layout.add_work_dialog, null);
                 // null 자리는 ViewGroup root
-                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this); // Activity가 Context를 상속받음
-                builder.setView(dialogView).show();
-                dialogDate = (EditText) dialogView.findViewById(R.id.dialog_date);
 
-                dialogDate.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        CalendarDatePickerDialogFragment cdp = new CalendarDatePickerDialogFragment()
-                                .setOnDateSetListener(MainActivity.this)
-                                .setFirstDayOfWeek(Calendar.SUNDAY);
-                        cdp.show(getSupportFragmentManager(), FRAG_TAG_DATE_PICKER);
+                DialogAddWork dialogAddWork = new DialogAddWork();
+                dialogAddWork.showDialog(dialogView);
 
-                    }
-                });
 
-                dialogTime = (EditText) dialogView.findViewById(R.id.dialog_time);
-
-                dialogTime.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        RadialTimePickerDialogFragment rtpd = new RadialTimePickerDialogFragment()
-                                .setOnTimeSetListener(MainActivity.this)
-                                .setForced12hFormat();
-                        rtpd.show(getSupportFragmentManager(), FRAG_TAG_TIME_PICKER);
-                    }
-                });
 
             }
         });
@@ -73,15 +40,6 @@ public class MainActivity extends AppCompatActivity implements CalendarDatePicke
     }
 
 
-    @Override
-    public void onDateSet(CalendarDatePickerDialogFragment dialog, int year, int monthOfYear, int dayOfMonth){
-        dialogDate.setText(dialog.getResources().getString(R.string.calendar_date_picker_result_values, year, monthOfYear, dayOfMonth));
-    }
-
-    @Override
-    public void onTimeSet(RadialTimePickerDialogFragment dialog, int hourOfDay, int minute){
-        dialogTime.setText(dialog.getResources().getString(R.string.calendar_time_picker_result_values, hourOfDay, minute));
-    }
 
 
 
