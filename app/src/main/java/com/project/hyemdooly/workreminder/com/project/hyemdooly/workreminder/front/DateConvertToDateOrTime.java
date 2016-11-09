de73ce5ba83ce4ba72fa4a78a492bb67afde8a4f
hyemdooly <hyemdooly@gmail.com>
2016-11-09 17:29:07 +0900

    DateConvertToDateOrTime Class add, listview_item.xml edit
    push notification

---
+++ app/src/main/java/com/project/hyemdooly/workreminder/com/project/hyemdooly/workreminder/front/DateConvertToDateOrTime.java
@@ -0,0 +1,26 @@
+package com.project.hyemdooly.workreminder.com.project.hyemdooly.workreminder.front;
+
+import java.util.Date;
+
+/**
+ * Created by songhyemin on 2016. 11. 9..
+ */
+
+public class DateConvertToDateOrTime {
+
+    private java.text.SimpleDateFormat dateFormat = new java.text.SimpleDateFormat("yyyy-MM-dd");
+    private java.text.SimpleDateFormat timeFormat = new java.text.SimpleDateFormat("HH : mm");
+
+    public DateConvertToDateOrTime(){
+    }
+
+
+    public String getTime(Date date){
+        return timeFormat.format(date);
+    }
+
+    public String getDate(Date date){
+        return dateFormat.format(date);
+    }
+
+}
