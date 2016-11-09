12e15eddff0db1d0bf01984b603f107846658d02
hyemdooly <hyemdooly@gmail.com>
2016-11-10 00:43:06 +0900

    add AlarmManager Class

---
+++ app/src/main/java/com/project/hyemdooly/workreminder/com/project/hyemdooly/workreminder/front/AlarmManagerActivity.java
@@ -0,0 +1,16 @@
+package com.project.hyemdooly.workreminder.com.project.hyemdooly.workreminder.front;
+
+import android.app.Activity;
+
+/**
+ * Created by songhyemin on 2016. 11. 10..
+ */
+
+public class AlarmManagerActivity extends Activity {
+
+    private static final String TAG = "AlarmManagerAcitivity";
+    private static final String INTENT_ACTION = "AlarmManagerAcitivity";
+
+
+
+}
